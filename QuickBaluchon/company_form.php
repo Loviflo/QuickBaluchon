11d3cbc768cfad503dc0dc5410933ce56d3970aa
kicass8 <kicass@live.fr>
2021-03-11 15:55:16 +0100

    Updated company form (got rid of test echos)

--- QuickBaluchon/company_form.php
+++ QuickBaluchon/company_form.php
@@ -1,13 +1,10 @@
 <?php
-//require_once('inc/conf.php');
-echo "ok";
+require_once('inc/conf.php');
 
-if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['SIRET_Nb']) && isset($_POST['motives'])){
-    echo "ok";
+if (isset($_POST['email']) && isset($_POST['SIRET_Nb']) && isset($_POST['motives'])){
     header("location:index.php?msg=\"Success\"");
     exit;
 }else{
-    echo "ko";
     header("location:index.php?msg=\"Error\"");
     exit;
 }
