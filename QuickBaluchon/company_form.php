672012625393e063ff17603765e26091b92a920e
kicass8 <kicass@live.fr>
2021-03-11 15:54:01 +0100

    Updated company form

--- QuickBaluchon/company_form.php
+++ QuickBaluchon/company_form.php
@@ -4,12 +4,10 @@ echo "ok";
 
 if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['SIRET_Nb']) && isset($_POST['motives'])){
     echo "ok";
-    header("index.php?msg=\"Success\"");
+    header("location:index.php?msg=\"Success\"");
     exit;
 }else{
     echo "ko";
-    header("index.php?msg=\"Error\"");
+    header("location:index.php?msg=\"Error\"");
     exit;
 }
-
-echo "ko2";
