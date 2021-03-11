8190439d9487323a86d7d47d44f125efeef15199
kicass8 <kicass@live.fr>
2021-03-11 15:50:04 +0100

    Updated company form

--- QuickBaluchon/company_form.php
+++ QuickBaluchon/company_form.php
@@ -2,5 +2,7 @@
 require_once('inc/conf.php');
 
 if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['SIRET_Nb']) && isset($_POST['motives'])){
-    header("index.php?msg=\"Ceci est un test\"");
+    header("index.php?msg=\"Success\"");
+}else{
+    header("index.php?msg=\"Error\"");
 }
