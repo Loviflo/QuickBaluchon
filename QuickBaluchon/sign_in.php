e45ab80059d48be0feb9cf8be1d540432a5d417c
kicass8 <kicass@live.fr>
2021-03-11 14:08:51 +0100

    Update sign_in

---
+++ QuickBaluchon/sign_in.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign_in page</title>
+    </head>
+    <?php include("inc/header.php"); ?>
+    <body>
+        <p>Want to join us ?</p>
+        <p>Are you ...</p>
+        <button type="button" class="btn btn-link"><a href="sign_in_company.php">A company</a></button>
+        <button type="button" class="btn btn-link"><a href="sign_in_delivery.php">A delivery man</a></button>
+    </body>
+    <?php include("inc/footer.php"); ?>
+</html>
