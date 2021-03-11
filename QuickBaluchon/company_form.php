9d130031b4d388c86ff3274c67fb3796131a7d72
kicass8 <kicass@live.fr>
2021-03-11 15:51:06 +0100

    Updated company form

--- QuickBaluchon/company_form.php
+++ QuickBaluchon/company_form.php
@@ -1,5 +1,6 @@
 <?php
 //require_once('inc/conf.php');
+echo "ok";
 
 if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['SIRET_Nb']) && isset($_POST['motives'])){
     header("index.php?msg=\"Success\"");
