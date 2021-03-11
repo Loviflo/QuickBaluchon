48a4c5ea41dd0268a644213d1125b1d3ff5eed1f
kicass8 <kicass@live.fr>
2021-03-11 14:13:44 +0100

    Update sign_in

--- QuickBaluchon/sign_in.php
+++ QuickBaluchon/sign_in.php
@@ -6,10 +6,20 @@
     </head>
     <?php include("inc/header.php"); ?>
     <body>
-        <p>Want to join us ?</p>
-        <p>Are you ...</p>
-        <button type="button" class="btn btn-link"><a href="sign_in_company.php">A company</a></button>
-        <button type="button" class="btn btn-link"><a href="sign_in_delivery.php">A delivery man</a></button>
+        <h2>Want to join us ?</h2>
+        <h3>Are you ...</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h4>A company ?</h4>
+                    <button type="button" class="btn btn-light"><a href="sign_in_company.php">Click here</a></button>
+                </div>
+                <div class="col-sm-5">
+                    <h4>A delivery man ?</h4>
+                    <button type="button" class="btn btn-link"><a href="sign_in_delivery.php">Click here</a></button>
+                </div>
+            </div>
+        </div>
     </body>
     <?php include("inc/footer.php"); ?>
 </html>
