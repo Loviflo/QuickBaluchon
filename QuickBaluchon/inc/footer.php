3055b9030d7f7097961e2c4f4740140889a03a85
kicass8 <kicass@live.fr>
2021-03-11 13:52:58 +0100

    Add inc folder and separated header and footer

---
+++ QuickBaluchon/inc/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <nav>
+        <ul>
+            <li><a href="Contact.php">Contact</a></li>
+            <li><a href="staff.php">WIP</a></li>
+        </ul>
+    </nav>
+</footer>
---
+++ QuickBaluchon/inc/header.php
@@ -0,0 +1,9 @@
+<header>
+    <a href="index.php"> <img src="Logo SPS.png" alt="Nothing to see here"></a>
+    <nav>
+        <ul>
+            <li><a href="sign_in.php">Sign_in</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
