377bf6bcbe5d4671da6c7b4040f5216d084b4a70
kicass8 <kicass@live.fr>
2021-03-11 14:40:33 +0100

    Update sign_in

--- QuickBaluchon/sign_in.php
+++ QuickBaluchon/sign_in.php
@@ -6,17 +6,17 @@
     </head>
     <?php include("inc/header.php"); ?>
     <body>
-        <div class="jumbotron text-center">
+        <div class="text-center">
             <h2>Want to join us ?</h2>
             <h3>Are you ...</h3>
             <div class="container">
                 <div class="row-cols-2">
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <h4>A company ?</h4>
                         <a href="sign_in_company.php">Click here</a>
                     </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <h4>A delivery man ?</h4>
                         <a href="sign_in_delivery.php">Click here</a>
                     </div>
