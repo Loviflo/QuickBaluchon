15955ee347009003ef8a0bdc882ff9459228acfc
kicass8 <kicass@live.fr>
2021-03-23 12:13:35 +0100

    Updated sql queries for deliveryman sign in + checks

--- QuickBaluchon/company_form.php
+++ QuickBaluchon/company_form.php
@@ -2,10 +2,10 @@
 require_once('bdd/database.php');
 
 if (isset($_POST['name']) && isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['siret']) && isset($_POST['motives'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $siret = $_POST['siret'];
-    $motives = $_POST['motives'];
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $siret = htmlspecialchars($_POST['siret']);
+    $motives = htmlspecialchars($_POST['motives']);
     $bdd = getDatabaseConnection();
     $q = 'INSERT INTO client (company_name,mail,siret_nb,motives) VALUES (:val1, :val2, :val3, :val4)';
     $req = $bdd->prepare($q);
