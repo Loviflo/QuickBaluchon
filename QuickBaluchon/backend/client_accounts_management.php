702879d18b7bc52040fd0419a5f9465d66e1fb76
Vivian <vivian.fr@free.fr>
2021-03-28 20:08:34 +0200

    Add modal for delete

--- QuickBaluchon/backend/client_accounts_management.php
+++ QuickBaluchon/backend/client_accounts_management.php
@@ -35,14 +35,42 @@ require_once(dirname(__DIR__) . "/bdd/database.php");
                         <td class="text-center">
                             <a href="/QuickBaluchon/QuickBaluchon/backend/client_account_management.php?company_name=<?php echo $user['company_name']; ?>" class="btn btn-primary" role="button" style="margin: 2px;"><i class="far fa-eye"></i></a>
                             <!-- <a class="btn btn-success" role="button" style="background: rgb(11,171,56);margin: 2px;"><i class="fas fa-pencil-alt"></i></a> -->
-                            <a href="/QuickBaluchon/QuickBaluchon/backend/actions/client_delete_account.php?company_name=<?php echo $user['company_name']; ?>" class="btn btn-danger" role="button" style="margin: 2px;"><i class="fas fa-trash"></i></a>
+                            <a href="/QuickBaluchon/QuickBaluchon/backend/actions/client_delete_account.php?company_name=<?php echo $user['company_name']; ?>" class="btn btn-danger" onclick="deletehref(this)" data-bs-toggle="modal" data-bs-target="#deleteAccount" role="button" style="margin: 2px;"><i class="fas fa-trash"></i></a>
                         </td>
                     </tr>
                     <?php } ?>
                 </tbody>
             </table>
         </ul>
+
+    <!-- Modal -->
+    <div class="modal fade" id="deleteAccount" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Suppresion de compte</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Etes-vous sûr de vouloir supprimer ce compte ?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                <a id="deleteURL" href=""><button type="button" class="btn btn-danger">Confirmer</button></a>
+            </div>
+            </div>
+        </div>
+    </div>
+
         <?php include(dirname(__DIR__) . "/inc/footer.php"); ?>
+
+        <script>
+            function deletehref(link) {
+                let href = link.href;
+                let idDeleteURL = document.getElementById('deleteURL');
+                idDeleteURL.href = href;
+            }
+        </script>
         
     </body>
 </html>
--- QuickBaluchon/backend/deliveryman_accounts_management.php
+++ QuickBaluchon/backend/deliveryman_accounts_management.php
@@ -35,14 +35,42 @@ require_once(dirname(__DIR__) . "/bdd/database.php");
                         <td class="text-center">
                             <a href="/QuickBaluchon/QuickBaluchon/backend/deliveryman_account_management.php?username=<?php echo $user['username']; ?>" class="btn btn-primary" role="button" style="margin: 2px;"><i class="far fa-eye"></i></a>
                             <!-- <a class="btn btn-success" role="button" style="background: rgb(11,171,56);margin: 2px;"><i class="fas fa-pencil-alt"></i></a> -->
-                            <a href="/QuickBaluchon/QuickBaluchon/backend/actions/deliveryman_delete_account.php?username=<?php echo $user['username']; ?>" class="btn btn-danger" role="button" style="margin: 2px;"><i class="fas fa-trash"></i></a>
+                            <a href="/QuickBaluchon/QuickBaluchon/backend/actions/deliveryman_delete_account.php?username=<?php echo $user['username']; ?>" class="btn btn-danger" onclick="deletehref(this)" data-bs-toggle="modal" data-bs-target="#deleteAccount" role="button" style="margin: 2px;"><i class="fas fa-trash"></i></a>
                         </td>
                     </tr>
                     <?php } ?>
                 </tbody>
             </table>
         </ul>
+
+        <!-- Modal -->
+        <div class="modal fade" id="deleteAccount" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Suppresion de compte</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Etes-vous sûr de vouloir supprimer ce compte ?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                <a id="deleteURL" href=""><button type="button" class="btn btn-danger">Confirmer</button></a>
+            </div>
+            </div>
+        </div>
+        </div>
+
         <?php include(dirname(__DIR__) . "/inc/footer.php"); ?>
+
+        <script>
+            function deletehref(link) {
+                let href = link.href;
+                let idDeleteURL = document.getElementById('deleteURL');
+                idDeleteURL.href = href;
+            }
+        </script>
         
     </body>
 </html>
